31db66ccbd96155bd80dd2e0da7645e8ab6d6a39
dark2strike <ds@ds.ds>
2020-07-08 23:03:22 +0300

    Minimal working

--- README.md
+++ README.md
@@ -1,17 +1,29 @@
 # habroxy
 Just test work for employment
 
-# Description
+### Description
 Proxy for getting habra pages by URI.
 
-# Requirements
+### Requirements
 Python 3.5+
+Flask
+Requests
+bs4(BeautifulSoup)
 
-# Installation
+### Installation
+```
 git clone https://github.com/itJunky/habroxy.git
 cd habroxy
 pip install -r requirements.txt
+```
 
-# Execute
+### Run
+```
 export FLASK_APP=habroxy.py
 python3 -m flask run
+```
+
+### Use
+Open in browser http://127.0.0.1:5000/ru/company/habr/blog/506348/
+
+And you can try set any another URI after http://127.0.0.1:5000/
--- common.py
+++ common.py
@@ -8,26 +8,39 @@ class Proxify:
         res = requests.get(url)
         return self.parse(res.text)
 
-    @staticmethod
-    def parse(text):
+    def parse(self, text):
         soup = BeautifulSoup(text)
-        buf_string = ''
         title = soup.findAll('span', {'class': 'post__title-text'})[0]
         new_title = soup.new_tag('span', {'class': 'post__title-text'})
+        new_title = self.replace(title, new_title)
+        title.replace_with(new_title)
 
-        for word in title.text.split():
+        content = soup.find_all('div', class_='post__text post__text-html post__text_v1')[0]
+        new_content = soup.new_tag('div', class_='post__text post__text-html post__text_v1')
+        # print('DEBUG: parse content: %s' % content)
+        new_content = self.replace(content, new_content)
+        # content.replace_width(new_content)
+        return soup.prettify("utf-8")
+
+    def replace(self, tag, new_tag):
+        buf_string = ''
+        # print('DEBUG: replace tag: %s' % tag)
+        # inner = BeautifulSoup(tag, features="lxml")
+        # print(inner)
+        # print("LENGTH: " + len(inner))
+        for word in tag.text.split():
             # TODO очищать от завершающих знаков припенания перед проверкой длины
-            print('SELECTED WORD: %s' % word)
+            print('DEBUG: replace word: %s' % word)
             if len(word) > 6:
+                print('DEBUG: big: %s' % word)
                 buf_string += word + '™ '
             else:
                 buf_string += word + ' '
 
-            new_title.string = buf_string
+            new_tag.string = buf_string
+            print('DEBUG: replace buf: %s' % buf_string)
 
-        title.replace_with(new_title)
-        return soup.prettify("utf-8")
-
-    # def replace(self, tag):
+        print('DEBUG: replace return: %s' % new_tag)
+        return new_tag
 
     # TODO подменять ссылки с оригинального хабра на собственный домен прокси
